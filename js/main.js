ac74f164f22a13f2afc4b8204c098a6747fb4ac1
Mc-zen <alphabetagamma11@hotmail.com>
2021-06-03 23:52:28 +0200

    fix rgb color

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
 
 
-function handleIntersect(entries, observer) {
-    console.log(34);
-    entries[0].target.classList.toggle("is-pinned", entries[0].intersectionRatio < 1)
+/*
+ *  observe when the nav-wrapper goes sticky
+ */
 
+function handleIntersect(entries, observer) {
+    entries[0].target.classList.toggle("is-pinned", entries[0].intersectionRatio < 1)
 }
+
 function onBodyLoad() {
     var IE = /*@cc_on!@*/false;
     try {
         const el = document.getElementsByClassName("nav-wrapper")[0];
-        console.log(el);
         const observer = new IntersectionObserver(handleIntersect, { threshold: [1] });
-        /*([e]) => e.target.classList.toggle("is-pinned", e.intersectionRatio < 1),
-        { threshold: [1] }
-    );*/
-
         observer.observe(el);
     } catch (e) {
 
     }
 }
 
+/*
+ * open/close navigation for mobile view
+ */
+
 function open_mobile_nav() {
     const el = document.getElementsByClassName("nav-inner-wrapper")[0];
     el.classList.add("nav-is-open");
 }
+
 function close_mobile_nav() {
     const el = document.getElementsByClassName("nav-inner-wrapper")[0];
     el.classList.remove("nav-is-open");
